34a58ac1cfac1702c2d4c5e1389cac12aaf030d9
juliacremus <juliacremus@gmail.com>
2024-01-24 10:25:45 -0300

    update testing reports

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Autoencoder coupled with a region mask
 
+## Relatório de Andamento
+
+### Dia 24 de janeiro de 2024
+
+* Objetivo:
+
+1. Verificar se a rede consegue realizar um noise removal dos dados de entrada
+2. Testar a regressão dos mesmos dados de entrada
+3. Testar utilizando somente MSE e MAE pra avaliação inicial, registrar os resultados.
+
+* Testes realizados:
+
+0. Teste #1:
+batch_size=64
+loss=mse
+metric=mse
+out=mask+fecg
+
+Mesmo utilizando o scheduler, quando usamos mais arquivos a loss aumenta e não vê-se aprendizado ao longo do tempo (mesmo fazendo a troca de LR_INIT). A loss caiu de 0.49 para 0.46 ao longo de 20 épocas de treinamento.
+
+1. Teste #2
+batch_size=32
+loss=mse
+metric=mse
+out=mask+fecg
+
+2. Máscara como entrada:
+batch_size=32
+loss=mse
+metric=mse
+out=fecg
+
+3. Máscara como entrada, loss modificada:
+batch_size=32
+loss=custom
+metric=mse
+out=fecg
+
+
+* Resultados obtidos: 
+
+* Ações a serem realizadas:
--- main.py
+++ main.py
@@ -12,11 +12,12 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow.keras.layers as Layers
+# import tensorflow.keras.layers as Layers, Input
 
 from models.linknet import linknet
 from losses.mse_with_mask import mse_with_mask
 from utils.lr_scheduler import callback
+from data_load import mask_as_input, signal_and_mask_as_output
 
 #%% Parameters
 
@@ -35,66 +36,17 @@ BATCH_SIZE = 32
 QRS_DURATION = 0.1  # seconds, max
 QRS_DURATION_STEP = 100
 
-INIT_LR = 0.1
-
-# data_store = np.empty(shape=[]) # shape=(EPOCHS, LEN_DATA, CHANNELS)
-# fecg_store = np.empty(shape=[]) # shape=(EPOCHS, LEN_DATA, 2)
+INIT_LR = 0.01
  
 #%% Data Loading 
 
+# data_store, fecg_store = mask_as_input.load_data(
+#     len_data = LEN_DATA, path = DATA_PATH, qrs_duration = QRS_DURATION
+# )
 
-for file in FILENAMES[0:2]:
-
-    file_info = mne.io.read_raw_edf(file)
-    filedata = file_info.get_data()
-    
-    max_absolute_value = np.max(np.abs(filedata[1::]))
-    
-    filedata *= 1 / max_absolute_value
-
-    annotations = mne.read_annotations(file)
-    time_annotations = annotations.onset
-
-
-    # Generates Binary masks
-
-    binary_mask = np.zeros(shape=file_info.times.shape)
-
-    for step in time_annotations:
-
-        center_index = np.where(file_info.times == step)[0][0]
-
-        qrs_region = np.where(
-            (file_info.times >= (step - QRS_DURATION)) &
-            (file_info.times <= (step + QRS_DURATION))
-        )[0]
-
-        binary_mask[qrs_region] = 1
-
-
-    for batch in range(0, np.shape(filedata)[1], LEN_DATA):
-
-
-        chunked_data = filedata[1::, (batch): ((batch + LEN_DATA))].transpose()
-        
-        chunked_fecg_real_data = filedata[0, (batch): (batch + LEN_DATA)]
-        chunked_fecg_binary_data = binary_mask[(batch): (batch + LEN_DATA)]
-
-        chunked_fecg_data = np.array([
-            chunked_fecg_real_data, 
-            chunked_fecg_binary_data
-        ]).transpose()
-
-
-        if batch == 0:
-
-            data_store = np.copy([chunked_data])
-            fecg_store = np.copy([chunked_fecg_data])
-
-        else:
-            data_store = np.vstack((data_store, [chunked_data]))
-            fecg_store = np.vstack((fecg_store, [chunked_fecg_data]))
-    
+data_store, fecg_store = signal_and_mask_as_output.load_data(
+    len_data = LEN_DATA, path = DATA_PATH, qrs_duration = QRS_DURATION
+)
 
 #%% Data Preprocessing
 
@@ -107,22 +59,30 @@ input_shape = (BATCH_SIZE, LEN_DATA, CHANNELS)
 
 model = linknet(input_shape, num_classes=1)
 
+# def weighted_loss(y_true, y_pred, weights):
+#       loss = tf.math.squared_difference(y_pred, y_true)
+#       w_loss = tf.multiply(weights, loss)
+#       return tf.reduce_mean(tf.reduce_sum(w_loss, axis=-1))
+  
+# targets = Input(BATCH_SIZE, LEN_DATA, 1)
+# out = Input(BATCH_SIZE, LEN_DATA, 1)
+# inp = Input(BATCH_SIZE, LEN_DATA, CHANNELS + 1)
 
-# %%
-
-# model.compile(optimizer='adam', loss=mse_with_mask, metrics=mse_with_mask)
+# model.add_loss(weighted_loss(targets, out, inp))
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=INIT_LR), 
-    loss=mse_with_mask, #tf.keras.losses.MSE, 
-    metrics=['mean_squared_error'])
+    loss=tf.keras.losses.MSE, #mse_with_mask, # 
+    metrics=['mean_squared_error']
+    )
 
 
 #%%
 
 history = model.fit(data_store, fecg_store, 
-          epochs=30, 
+          epochs=60, 
           batch_size=BATCH_SIZE,
+          validation_split=0.2,
           shuffle=True, 
           callbacks=[callback],
     )
@@ -180,6 +140,16 @@ for file in FILENAMES[3:4]:
             fecg_store_test = np.vstack((fecg_store_test, [chunked_fecg_data]))
     
 
+#%%
+teste = model.predict(data_store, 64)
+# %%
 
-teste = model.predict(data_store, 32)
+fig, ax = plt.subplots()
+
+ax.plot(fecg_store[6, :, 1])
+
+ax1 = ax.twinx()
+
+ax1.plot(teste[6, :, 1], color='orange')
+
 # %%
