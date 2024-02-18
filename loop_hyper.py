5197c7e8772fe45d780f73666ff12b88360ac158
juliacremus <juliacremus@gmail.com>
2024-02-18 19:42:26 -0300

    remove the loop in lr constant

--- loop_hyper.py
+++ loop_hyper.py
@@ -34,67 +34,71 @@ MODEL_INPUT_SHAPE = (BATCH_SIZE, LEN_BATCH, CHANNELS)
 
 #%% loop in variables
 
-for lr in np.arange(LOWER_LIMIT_LR, UPPER_LIM_LR + LR_STEP, LR_STEP):
-    
-    for w_mask in np.arange(0, 1.005, 0.05):
+
+for w_mask in np.arange(0.4, 1.1, 0.1):
         
-        w_signal_upper_bound = 1 - w_mask
+    w_signal_upper_bound = 1 - w_mask
+
+    for w_signal in np.arange(0, w_signal_upper_bound + 0.1, 0.1):
+
+        w_combined = 1 - w_mask - w_signal
+
+
+        # for i in range(0, TOTAL_FILES, 1):
         
-        for w_signal in np.arange(0, w_signal_upper_bound + 0.05, 0.05):
-            
-            w_combined = 1 - w_mask - w_signal
+        i = 4
+
+        prefix_id = f'LR_{UPPER_LIM_LR}-W_MASK_{w_mask}-W_SIG_{w_signal}-LEFT_{i}'
         
-            
-            for i in range(0, TOTAL_FILES, 1):
+        print(prefix_id)
         
-                prefix_id = f'LR_{lr}-W_MASK_{w_mask}-W_SIG_{w_signal}-LEFT_{i}'
-        
-                print(prefix_id)
-        
-                    
-                training_data, testing_data = data_loader(
-                        DATA_PATH, 
-                        LEN_BATCH, 
-                        QRS_DURATION, 
-                        QRS_DURATION_STEP,
-                        leave_for_testing=i,
-                        type_of_file='edf'
-                )
-            
-                model = ProposedAE(
-                    MODEL_INPUT_SHAPE, 
-                    BATCH_SIZE, 
-                    lr, 
-                    w_mask, 
-                    w_signal, 
-                    w_combined, 
-                    training_data=training_data[0], 
-                    ground_truth=training_data[1],
-                    testing_data=testing_data[0], 
-                    ground_truth_testing=testing_data[1]
-                )
-                
-                history, testing_metrics, predict = model.fit_and_evaluate()
-                
-                
-                print(prefix_id)
-                print(testing_metrics)
-                
-                
-                # save things
-                
-                this_dir = os.path.join(RESULTS_PATH, prefix_id)
-                
-                 
-                os.mkdir(this_dir)
-                
-                pd.DataFrame.from_dict(history.history).to_csv(this_dir + '/' + prefix_id + '-training_history.csv')
-                
-                index = 0
-                for batch_pred in predict:
-                    np.savetxt(this_dir + '/' + prefix_id + f'-prediction_{index}.csv', batch_pred, delimiter=',')
-                    index += 1
-                
-                del history
-                del predict  
+
+        training_data, testing_data = data_loader(
+                DATA_PATH, 
+                LEN_BATCH, 
+                QRS_DURATION, 
+                QRS_DURATION_STEP,
+                leave_for_testing=i,
+                type_of_file='edf'
+        )
+
+        model = ProposedAE(
+            MODEL_INPUT_SHAPE, 
+            BATCH_SIZE, 
+            UPPER_LIM_LR, 
+            w_mask, 
+            w_signal, 
+            w_combined, 
+            training_data=training_data[0], 
+            ground_truth=training_data[1],
+            testing_data=testing_data[0], 
+            ground_truth_testing=testing_data[1]
+        )
+
+        history, testing_metrics, predict = model.fit_and_evaluate()
+
+
+        print(prefix_id)
+        print(testing_metrics)
+
+
+        # save things
+
+        this_dir = os.path.join(RESULTS_PATH, prefix_id)
+
+
+        os.mkdir(this_dir)
+
+        pd.DataFrame.from_dict(history.history).to_csv(this_dir + '/' + prefix_id + '-training_history.csv')
+
+        index = 0
+        for batch_pred in predict:
+            np.savetxt(this_dir + '/' + prefix_id + f'-prediction_{index}.csv', batch_pred, delimiter=',')
+            index += 1
+
+        del training_data
+        del testing_data
+        del model
+        del history
+        del predict  
 # %%
