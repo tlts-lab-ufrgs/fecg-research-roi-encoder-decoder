fe55146a45fb26ba2b9e635df0215a6cb852df2d
juliacremus <juliacremus@gmail.com>
2024-03-04 16:23:18 -0300

    separate confidence interval in utils

---
+++ utils/mean_confidence_interval.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy.stats
+
+
+def mean_confidence_interval(data, name='', confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.norm.ppf((1 + confidence) / 2.)
+    
+    to_return = f'{name} \n {round(m * 100, 4)} $\pm$ {round(se * 100, 4)} ({round((m-h) * 100, 4)} - {round((m+h) * 100, 4)})'
+    
+    return to_return
