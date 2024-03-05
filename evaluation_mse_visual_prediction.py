fix metrics calculation errors

evaluation_mse_visual_prediction.py
@@ -15,6 +15,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 from data_load.load_leave_one_out import data_loader
+from utils.mean_confidence_interval import mean_confidence_interval
     
 #%% constants 
 
@@ -22,8 +23,8 @@ from data_load.load_leave_one_out import data_loader
 RESULTS_PATH = "/home/julia/Documents/fECG_research/research_dev/autoencoder_with_mask/results/"
 DATA_PATH =  "/home/julia/Documents/fECG_research/datasets/abdominal-and-direct-fetal-ecg-database-1.0.0/"
 
-CHANNELS = 4
-LEN_BATCH = 1024
+CHANNELS = 3
+LEN_BATCH = 512
 QRS_DURATION = 0.1  # seconds, max
 QRS_DURATION_STEP = 50
 
@@ -38,6 +39,91 @@ def mse_function(y_true, y_pred):
     )
     
     return mse_value
+
+to_remove = [
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    52,
+    56,
+    57,
+    58,
+    59,
+    60,
+    177,
+    179,
+    180,
+    181,
+    182,
+    183,
+    184,
+    185,
+    186,
+    187,
+    188,
+    189,
+    190,
+    197,
+    198,
+    199,
+    200,
+    201,
+    202,
+    203,
+    204,
+    205,
+    206,
+    207,
+    311,
+    312,
+    313,
+    314,
+    315,
+    316,
+    317,
+    318,
+    319,
+    320,
+    335, 
+    336, 
+    338,
+    339,
+    340,
+    341,
+    343,
+    365,
+    371,
+    393,
+    394,
+    395,
+    396,
+    397,
+    398,
+    399,
+    400,
+    401,
+    402,
+    403,
+    404,
+    405,
+    406,
+    407,
+    408,
+    409,
+    410,
+    411,
+    412,
+]
+
 
 #%% data load
 
@@ -64,12 +150,17 @@ for i in range(5):
     
     testing_data[i] = {
         'signal': fecg_testing_data,
-        'roi_signal': fecg_roi
+        'roi_signal': fecg_roi, 
+        'binary_true_mask': np.where(
+            fecg_testing_data[:, :, 1] == 0, 
+            0, 
+            1
+        )
     }
 
 
 #%% concat results of the same dir
-results_dir = glob.glob(RESULTS_PATH + '020324-LEN_DATA_1024-3CH-VAL_LOSS-MOD_DA6-LR_0.0001*')
+results_dir = glob.glob(RESULTS_PATH + '010324-3CH-VAL_LOSS-MOD_DA6-LR_0.0001*')
 results_rows = []
 
 for i in results_dir:
@@ -91,14 +182,21 @@ for i in results_dir:
         prediction_index = int(file.split('-prediction_')[1].split('-')[0].replace('.csv', ''))
       
         
+        if test_file == 4 and int(prediction_index) in to_remove:
+            continue
+        
         prediction_data = pd.read_csv(file, names=['signal', 'mask'])
         prediction_data['binary_mask'] = prediction_data['mask'].where(prediction_data['mask'] == 0, 1)
-        prediction_data['combined'] = prediction_data['signal'] * prediction_data['binary_mask']
         
+        
+        prediction_data['combined'] = prediction_data['signal'] * prediction_data['binary_mask']
+        prediction_data['roi_signal'] = prediction_data['signal'] * testing_data[test_file]['binary_true_mask'][prediction_index]
         
         mse_signal_partial = mse_function(testing_data[test_file]['signal'][prediction_index, :, 0], prediction_data['signal'])
         mse_mask_partial = mse_function(testing_data[test_file]['signal'][prediction_index, :, 1], prediction_data['mask'])
-        mse_combined_partial = mse_function(testing_data[test_file]['roi_signal'][prediction_index], prediction_data['combined'])
+        mse_combined_partial = mse_function(
+            testing_data[test_file]['roi_signal'][prediction_index], 
+            prediction_data['roi_signal'])
            
         mse_signal += mse_signal_partial
         mse_mask += mse_mask_partial
@@ -106,13 +204,15 @@ for i in results_dir:
         
     
 
-        r_squared_signal_partial = r2_score(testing_data[test_file]['signal'][prediction_index, :, 0], prediction_data['signal'])
+        r_squared_signal_partial = (r2_score(testing_data[test_file]['signal'][prediction_index, :, 0], prediction_data['signal']))
         r_squared_mask_partial = r2_score(testing_data[test_file]['signal'][prediction_index, :, 1], prediction_data['mask'])
-        r_squared_combined_partial = r2_score(testing_data[test_file]['roi_signal'][prediction_index], prediction_data['combined'])
+        r_squared_combined_partial = r2_score(
+            testing_data[test_file]['roi_signal'][prediction_index], 
+            prediction_data['roi_signal'])
            
-        r_squared_signal += abs(r_squared_signal_partial)
-        r_squared_mask += abs(r_squared_mask_partial)
-        r_squared_combined += abs(r_squared_combined_partial)
+        r_squared_signal += (r_squared_signal_partial)
+        r_squared_mask += (r_squared_mask_partial)
+        r_squared_combined += (r_squared_combined_partial)
         
         # false_positive = [
         # ]
@@ -120,11 +220,11 @@ for i in results_dir:
         # if prediction_index in [int(i / 512) for i in false_positive] and test_file == 0:
            
         if prediction_index in [
-            1  
+            1
         ]: 
             fig, ax = plt.subplots()
             
-            ax.set_title('')
+            # ax.set_title('')
             # ax.set_title(f'W mask {w_mask}, W signal {w_signal} - {test_file}')
             
  
@@ -182,15 +282,50 @@ for i in results_dir:
 #%% form data frame
 
 metrics_dataframe = pd.DataFrame(
-    np.array(results_rows), columns=['test_file', 'w_mask', 'w_signal', 'mse_signal', 'mse_mask', 'mse_combined', 'r2_signal', 'r2_mask', 'r2_combined']
+    np.array(results_rows), 
+    columns=[
+        'test_file', 
+        'w_mask', 
+        'w_signal', 
+        'mse_signal', 
+        'mse_mask', 
+        'mse_combined', 
+        'r2_signal', 
+        'r2_mask', 
+        'r2_combined']
 )
 
 #%%
 
 metrics_dataframe.sort_values(by = ['mse_mask'], inplace=True)
 
+# #%%
+
+# a = metrics_dataframe.groupby(['w_mask', 'w_signal']).mean()
+
 #%%
 
-a = metrics_dataframe.groupby(['w_mask', 'w_signal']).mean()
+print(mean_confidence_interval(
+    metrics_dataframe['mse_signal'].values, 'MAE Signal'
+))
 
-#%%
+print(mean_confidence_interval(
+    metrics_dataframe['mse_mask'].values, 'MAE Mask'
+))
+
+print(mean_confidence_interval(
+    metrics_dataframe['mse_combined'].values, 'MAE RoI'
+))
+
+print(mean_confidence_interval(
+    metrics_dataframe['r2_signal'].values, 'R2 Signal'
+))
+
+print(mean_confidence_interval(
+    metrics_dataframe['r2_mask'].values, 'R2 Mask'
+))
+
+print(mean_confidence_interval(
+    metrics_dataframe['r2_combined'].values, 'R2 RoI'
+))
+# %%

evaluation_peak_detection.py
@@ -45,14 +45,14 @@ def mse_function(y_true, y_pred):
 
 #%% constants 
 
-FILES_TO_CALCULATE = '040324-RESAMPLED_SIGNAL_250-LR_0.0001'
+FILES_TO_CALCULATE = '050324-3CH-DROPOUTMOD-LR_0.0001'
 
 # [w_mask, w_signal]
 WEIGHTS_TO_EVAL = [
     [0.3, 0.1]
 ] 
 
-SAMPLING_FREQ = 250
+SAMPLING_FREQ = 1000
 
 CHANNELS = 3
 RESAMPLING_FREQUENCY_RATIO = int(1000 / SAMPLING_FREQ)
@@ -69,6 +69,8 @@ QRS_DURATION = 0.1  # seconds, max
 QRS_DURATION_STEP = int(50 / RESAMPLING_FREQUENCY_RATIO)
 MIN_QRS_DISTANCE = int(300 / RESAMPLING_FREQUENCY_RATIO) # fs = 1000Hz
 MASK_MIN_HEIGHT = 0.7
+
+LIMIT = int(300000 / RESAMPLING_FREQUENCY_RATIO)# - LEN_BATCH
 
 #%% data load
 
@@ -222,8 +224,7 @@ for w in WEIGHTS_TO_EVAL:
             
             prediction_index = int(file.split('-prediction_')[1].split('-')[0].replace('.csv', ''))
         
-            
-            if j == 4 and int(prediction_index / RESAMPLING_FREQUENCY_RATIO) in to_remove:
+            if j == 4 and int(prediction_index) in to_remove:
                 continue
             
             prediction_data = pd.read_csv(file, names=['signal', 'mask'])
@@ -292,14 +293,12 @@ for j in range(NUMBER_OF_FILES):
     false_positive_pt = 0
     false_negative_pt = 0
 
-    limit = 29696# 149760
-
     for peak in annotations_data[f'{j}'] * SAMPLING_FREQ:
         
-        if j == 4 and int(np.floor(peak / LEN_BATCH) * RESAMPLING_FREQUENCY_RATIO) in to_remove:
+        if j == 4 and int(np.floor(peak / LEN_BATCH)) in to_remove:
             continue
         
-        if peak <= limit:
+        if peak <= LIMIT:
             
             total_peaks += 1
         
@@ -358,7 +357,6 @@ for j in range(NUMBER_OF_FILES):
                 false_positive += 1
             
         already_mentioned_peaks.append(peak)
-    # print(total_peaks)
     
     already_counted = []
     

loop_hyper.py
@@ -6,6 +6,7 @@ import pandas as pd
 import tensorflow as tf
 from keras import backend as K
 from numba import cuda
+import matplotlib.pyplot as plt
 
 from data_load.load_leave_one_out import data_loader
 from models.ae_proposed import ProposedAE
@@ -27,11 +28,13 @@ RESULTS_PATH = "/home/julia/Documents/fECG_research/research_dev/autoencoder_wit
 DATA_PATH =  "/home/julia/Documents/fECG_research/datasets/abdominal-and-direct-fetal-ecg-database-1.0.0/"
 
 CHANNELS = 3
-LEN_BATCH = 1024
+LEN_BATCH = 512
 QRS_DURATION = 0.1  # seconds, max
-QRS_DURATION_STEP = 50
+QRS_DURATION_STEP = 12
 
 MODEL_INPUT_SHAPE = (BATCH_SIZE, LEN_BATCH, CHANNELS)
+
+RESAMPLE_FREQ_RATIO = 1
 
 
 #%% loop in variables
@@ -57,12 +60,11 @@ w_signal = 0.1
 w_combined = 1 - w_mask - w_signal
 
 
-# for i in range(TOTAL_FILES - 1, -1, -1):
-for i in range(0, TOTAL_FILES, 1):
+for i in range(0,1, 1):
     
     # i = 4
 
-    prefix_id = f'020324-LEN_DATA_1024-3CH-VAL_LOSS-MOD_DA6-LR_{UPPER_LIM_LR}-W_MASK_{w_mask}-W_SIG_{w_signal}-LEFT_{i}'
+    prefix_id = f'050324-3CH-DROPOUTMOD-LR_{UPPER_LIM_LR}-W_MASK_{w_mask}-W_SIG_{w_signal}-LEFT_{i}'
     
     
     print(prefix_id)
@@ -74,9 +76,10 @@ for i in range(0, TOTAL_FILES, 1):
             QRS_DURATION, 
             QRS_DURATION_STEP,
             leave_for_testing=i,
-            type_of_file='edf'
+            type_of_file='edf', 
+            resample_fs=RESAMPLE_FREQ_RATIO
     )
-
+    
     model = ProposedAE(
         MODEL_INPUT_SHAPE, 
         BATCH_SIZE, 
@@ -117,14 +120,6 @@ for i in range(0, TOTAL_FILES, 1):
     del model
     del history
     del predict  
-    
-    K.clear_session()
-    tf.compat.v1.reset_default_graph()
-    # try:
-    #     cuda.select_device(0)
-    #     cuda.close()
-    # except:
-    #     print('cuda retunr an error')
 
 
 # %%

utils/mean_confidence_interval.py
@@ -8,6 +8,6 @@ def mean_confidence_interval(data, name='', confidence=0.95):
     m, se = np.mean(a), scipy.stats.sem(a)
     h = se * scipy.stats.norm.ppf((1 + confidence) / 2.)
     
-    to_return = f'{name} \n {round(m * 100, 4)} $\pm$ {round(se * 100, 4)} ({round((m-h) * 100, 4)} - {round((m+h) * 100, 4)})'
+    to_return = f'{name} \n {round(m, 4)} $\pm$ {round(se, 4)} ({round((m-h), 4)} - {round((m+h), 4)})'
     
     return to_return