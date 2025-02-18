92db8225841e21acd210c49feb32702abda4e883
juliacremus <juliacremus@gmail.com>
2025-02-18 15:47:22 -0300

    add code to help text writing

--- utils/stats_fn.py
+++ utils/stats_fn.py
@@ -2,11 +2,15 @@ import numpy as np
 import scipy.stats
 
 
-def mean_confidence_interval(data, name='', confidence=0.95):
+def mean_confidence_interval(data, name='', confidence=0.95, percentage=False):
     a = 1.0 * np.array(data)
     n = len(a)
     m, se = np.mean(a), scipy.stats.sem(a)
     h = se * scipy.stats.norm.ppf((1 + confidence) / 2.)
+
+    if percentage:
+        m *= 100
+        se *= 100
     
     to_return = f'{name} \n {round(m, 4)} $\pm$ {round(se, 4)} ({round((m-h), 4)} - {round((m+h), 4)})'
     
