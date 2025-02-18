9b42ebd9b0845bdf1d60ec92e946578b45322d61
juliacremus <juliacremus@gmail.com>
2025-02-18 15:50:19 -0300

    test modifications

--- loop_hyper.py
+++ loop_hyper.py
@@ -9,19 +9,19 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 from data_load.data_loader import DataLoader
-from models.ae_proposed_3blocks import ProposedAE
+from models.ae_proposed_rev0 import ProposedAE
 
 #%% To run other experiments please change this below
 
 # CHANGEBLE VARIABLES ---------------------------------------------------------------------------------------------- 
-TOTAL_FILES = 12
+TOTAL_FILES = 5
 CHANNELS = 3
 RESAMPLE_FREQ_RATIO = 2
 HAVE_DIRECT_FECG = True
 
 RESULTS_PATH = "/home/julia/Documents/research/sprint_1/results/ablation_extended/"
-DATA_PATH = "/home/julia/Documents/research/datasets/b2-records/B2_Labour_dataset/"
-# DATA_PATH =  "/home/julia/Documents/research/datasets/abdominal-and-direct-fetal-ecg-database-1.0.0/"
+# DATA_PATH = "/home/julia/Documents/research/datasets/b2-records/B2_Labour_dataset/"
+DATA_PATH =  "/home/julia/Documents/research/datasets/abdominal-and-direct-fetal-ecg-database-1.0.0/"
 # -------------------------------------------------------------------------------------------------------------------
 
 #%% Model constants
@@ -34,7 +34,7 @@ QRS_DURATION_STEP = int(50 / RESAMPLE_FREQ_RATIO)
 
 MODEL_INPUT_SHAPE = (BATCH_SIZE, LEN_BATCH, CHANNELS)
 
-TYPE_OF_FILE = 'txt'
+TYPE_OF_FILE = 'edf'
 
 w_mask = 0.3
 w_signal = 0.1
@@ -64,11 +64,19 @@ data_loader = DataLoader(
 
 #%% Loop in files to run cross-validation 
 
-for i in  range(0, TOTAL_FILES):
+for i in  range(0,TOTAL_FILES):
     
-    prefix_id = f'{today}-MASK_{type_of_mask}-DECODER_BY_{decoder_type}-ED_rev2-B2-500hz-LR_{UPPER_LIM_LR}-W_MASK_{w_mask}-W_SIG_{w_signal}-LEFT_{i}'
+    prefix_id = f'{today}-MASK_{type_of_mask}-DECODER_BY_{decoder_type}-200Hz-ABCD-LR_{UPPER_LIM_LR}-W_MASK_{w_mask}-W_SIG_{w_signal}-LEFT_{i}'
     
     print(prefix_id)
+
+        # save things
+
+    this_dir = os.path.join(RESULTS_PATH, prefix_id)
+    os.mkdir(this_dir)
+
+    # os.mkdir(this_dir + '/model')
+    # os.mkdir(this_dir + '/qz_model')
 
     training_data, testing_data = data_loader.data_load(i)
        
@@ -81,25 +89,18 @@ for i in  range(0, TOTAL_FILES):
         w_combined, 
         training_data=training_data[0], 
         ground_truth=training_data[1],
-        testing_data=testing_data[0][0::1], 
-        ground_truth_testing=testing_data[1][0::1], 
-        epochs=NUMBER_OF_EPOCHS
+        testing_data=testing_data[0], 
+        ground_truth_testing=testing_data[1], 
+        epochs=NUMBER_OF_EPOCHS, 
+        # saved_model_dir=this_dir
     )
 
     history, testing_metrics, predict = model.fit_and_evaluate()
 
 
-    print(prefix_id)
-    print(testing_metrics)
+    # print(prefix_id)
+    # print(testing_metrics)
 
-
-    # save things
-
-    this_dir = os.path.join(RESULTS_PATH, prefix_id)
-
-    #model.save(this_dir + '-model')
-
-    os.mkdir(this_dir)
 
     pd.DataFrame.from_dict(history.history).to_csv(this_dir + '/' + prefix_id + '-training_history.csv')
 
